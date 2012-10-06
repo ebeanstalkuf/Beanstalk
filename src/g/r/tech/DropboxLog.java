ab52b747617e1959f5d417cfa79bc4723b9003c2
ebeanstalkuf <egonzalez@ufl.edu>
2012-10-05 23:37:08 -0400

    from ef

--- src/g/r/tech/DropboxLog.java
+++ src/g/r/tech/DropboxLog.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 //testing testing
 //testing
 //testing3
+//testing4
 public class DropboxLog extends Activity {
     /** Called when the activity is first created. */
     @Override
