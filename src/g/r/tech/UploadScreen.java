14f44e5fd411f8afae7a943ee4e739ac207192b6
ebeanstalkuf <egonzalez@ufl.edu>
2012-10-11 10:44:59 -0400

    'cloud is moving...cloud is moving'

--- src/g/r/tech/UploadScreen.java
+++ src/g/r/tech/UploadScreen.java
@@ -1,13 +1,76 @@
 package g.r.tech;
 
+
 import android.app.Activity;
+import android.content.ClipData;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.DragShadowBuilder;
+import android.view.View.OnDragListener;
+import android.view.View.OnTouchListener;
+import android.widget.LinearLayout;
 
 public class UploadScreen extends Activity {
     /** Called when the activity is first created. */
-    @Override
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.upload);
+        
+        findViewById(R.id.Upcloud).setOnTouchListener(new MyTouchListener());
     }
+    
+    private final class MyTouchListener implements OnTouchListener {
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+          if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            ClipData data = ClipData.newPlainText("", "");
+            DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+            view.startDrag(data, shadowBuilder, view, 0);
+            view.setVisibility(View.VISIBLE);
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }
+    
+    class MyDragListener implements OnDragListener {
+        
+        Drawable movingCloud = getResources().getDrawable(R.drawable.upcloud);
+
+       
+    	public boolean onDrag(View v, DragEvent event) {
+          int action = event.getAction();
+          switch (event.getAction()) {
+          case DragEvent.ACTION_DRAG_STARTED:
+            // Do nothing
+            break;
+          case DragEvent.ACTION_DRAG_ENTERED:
+            v.setBackgroundDrawable(movingCloud);
+            break;
+          case DragEvent.ACTION_DRAG_EXITED:
+            v.setBackgroundDrawable(movingCloud);
+            break;
+          case DragEvent.ACTION_DROP:
+            // Dropped, reassign View to ViewGroup
+            View view = (View) event.getLocalState();
+            ViewGroup owner = (ViewGroup) view.getParent();
+            owner.removeView(view);
+            LinearLayout container = (LinearLayout) v;
+            container.addView(view);
+            view.setVisibility(View.VISIBLE);
+            break;
+          case DragEvent.ACTION_DRAG_ENDED:
+            v.setBackgroundDrawable(movingCloud);
+          default:
+            break;
+          }
+          return true;
+        }
+      }
 }
