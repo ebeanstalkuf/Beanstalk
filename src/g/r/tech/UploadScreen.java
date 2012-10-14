119a7d5e447b212c3383dcf76fd9d84cda913d6f
Brian Lam <blam0707@gmail.com>
2012-10-14 18:38:30 -0400

    Edited UploadScreen to implement upload
    Has not fully integrated yet

--- src/g/r/tech/UploadScreen.java
+++ src/g/r/tech/UploadScreen.java
@@ -3,20 +3,33 @@ package g.r.tech;
 
 import android.app.Activity;
 import android.content.ClipData;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-
 import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.DragShadowBuilder;
 import android.view.View.OnDragListener;
 import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.session.AccessTokenPair;
+import com.dropbox.client2.session.AppKeyPair;
+import com.dropbox.client2.session.Session.AccessType;
 
 public class UploadScreen extends Activity {
     /** Called when the activity is first created. */
+	DropboxAPI<AndroidAuthSession> dropApi;
+    final static private String APP_KEY = "dhgel7d3dcsen3d";
+    final static private String APP_SECRET = "evnp2bxtokmy7yy";
+    final static private AccessType ACCESS_TYPE = AccessType.APP_FOLDER;
+    final static private String ACCOUNT_PREFS_NAME = "prefs";
+    final static private String ACCESS_KEY_NAME = "ACCESS_KEY";
+    final static private String ACCESS_SECRET_NAME = "ACCESS_SECRET";
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +51,40 @@ public class UploadScreen extends Activity {
           }
         }
       }
+    
+    public void uploadFile()
+    {
+    	
+    }
+    
+    private AndroidAuthSession buildSession() {
+        AppKeyPair appKeyPair = new AppKeyPair(APP_KEY, APP_SECRET);
+        AndroidAuthSession session;
+
+        String[] stored = getKeys();
+        if (stored != null) {
+            AccessTokenPair accessToken = new AccessTokenPair(stored[0], stored[1]);
+            session = new AndroidAuthSession(appKeyPair, ACCESS_TYPE, accessToken);
+        } else {
+            session = new AndroidAuthSession(appKeyPair, ACCESS_TYPE);
+        }
+
+        return session;
+    }
+    
+    private String[] getKeys() {
+        SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+        String key = prefs.getString(ACCESS_KEY_NAME, null);
+        String secret = prefs.getString(ACCESS_SECRET_NAME, null);
+        if (key != null && secret != null) {
+        	String[] ret = new String[2];
+        	ret[0] = key;
+        	ret[1] = secret;
+        	return ret;
+        } else {
+        	return null;
+        }
+    }
     
     class MyDragListener implements OnDragListener {
         
