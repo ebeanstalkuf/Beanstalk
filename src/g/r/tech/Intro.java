71174219aba4ede36b60843f556d074f57f5a726
ebeanstalkuf <egonzalez@ufl.edu>
2012-10-28 15:05:47 -0400

    'restore'

--- src/g/r/tech/Intro.java
+++ src/g/r/tech/Intro.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-
+//restore
 //lets try a test here ;)
 public class Intro extends Activity {
     /** Called when the activity is first created. */
